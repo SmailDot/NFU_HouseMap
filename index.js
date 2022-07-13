4441a3dd1b892e06407316a3f487c621bdcc497b
Dot <s9413783@gmail.com>
2022-07-14 07:34:41 +0800

    Insomnia website erection exercise

---
+++ index.js
@@ -0,0 +1,9 @@
+window.alert("歡迎 有任何想法歡迎提出")
+window.addEventListener("load",function()
+{
+    const btn1=document.getElementById('ben')
+    btn1.addEventListener('click',function(){
+        console.log('聯繫我')
+    })
+}
+)
